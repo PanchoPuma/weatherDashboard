edf2ab69b457df7d822c31df25b19a5e3d40cf0c
Francisco Puma <francis_pancho12@hotmail.com>
2021-05-21 22:31:43 -0700

    reworked it with a different API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,16 @@
 // variables
 var APIkey = "b1d0aae8e0d8743f9981d6a87d77d2bb";
-var lat = "-0.22985"
-var lon = "78.52495"
-var openWeatherAPI = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=metric&exclude=daily&appid="+ APIkey
+var city = "Quito"
+var openWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&exclude=daily&appid="+ APIkey
 
 fetch (openWeatherAPI).then(function(response) {
     if (response.ok) {
         response.json().then(function (details){
             console.log (details)// all details
-            console.log (details.current.temp) //current temperature
-            console.log (details.current.humidity) //current Humidity
-            console.log (details.current.wind_speed) //current Wind Speed
-            console.log (details.current.uvi) //current UV index
+            console.log (details.name)// name of the city 
+            console.log (details.main.temp) //current temperature
+            console.log (details.main.humidity) //current Humidity
+            console.log (details.wind.speed) //current Wind Speed
         })
     }
 })
-
-
-// original API link 
-//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
