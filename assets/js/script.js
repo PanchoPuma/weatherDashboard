a9c27554e7b8ea35616b1c33c91d4ebe88b3b25a
Francisco Puma <francis_pancho12@hotmail.com>
2021-05-21 22:26:24 -0700

    initial testing with API with Quito

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,16 @@
 var APIkey = "b1d0aae8e0d8743f9981d6a87d77d2bb";
 var lat = "-0.22985"
 var lon = "78.52495"
-var frequency = "daily"
-var openWeatherAPI = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude="+frequency+"&appid="+ APIkey
+var openWeatherAPI = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=metric&exclude=daily&appid="+ APIkey
 
 fetch (openWeatherAPI).then(function(response) {
     if (response.ok) {
         response.json().then(function (details){
-            console.log (details)
+            console.log (details)// all details
+            console.log (details.current.temp) //current temperature
+            console.log (details.current.humidity) //current Humidity
+            console.log (details.current.wind_speed) //current Wind Speed
+            console.log (details.current.uvi) //current UV index
         })
     }
 })
